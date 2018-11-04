d5ad2cbcd9f34a75403b975f38a45e3b955db522
Christine Manrique <tinemanrique@gmail.com>
2018-11-04 16:54:14 +0800

    Updates on responsiveness

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import about from '../assets/images/undraw_resume.png';
-import work from '../assets/images/undraw_work2.png';
+import work from '../assets/images/undraw_work.png';
 import blog from '../assets/images/undraw_blog.png';
 import contact from '../assets/images/undraw_contact.png';
 
